10de5b81d49e4765080b34d8899f231049bb79ff
Kavya765 <kavyarastogi765@gmail.com>
2026-01-20 10:40:57 +0530

    added exp2

---
+++ exp-1/src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+export default function ButtonBasic() {
+  return (
+    <>
+    <Button size="small" variant='outlined'>Small</Button>
+    <Button size="medium" variant='contained'>Medium</Button>
+<TextField id="outlined-basic" label="Outlined" variant="outlined" />
+</>
+) 
+
+}
--- exp-1/src/components/spa.jsx
+++ exp-1/src/components/spa.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './home';
 import About from './about';
 import Contact from './Contact';
-
+import ButtonBasic from './Button';
 export default function SinglePageApp() {
   return (
     <BrowserRouter>
       <nav>
         <Link to="/home">Home</Link> |{" "}
         <Link to="/about">About</Link> |{" "}
-        <Link to="/Contact">Contact</Link>
+        <Link to="/Contact">Contact</Link> |{" "}
+        <Link to="/ButtonBasic">Button</Link> |{" "}
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
                <Route path="/Contact" element={<Contact />} />
+               <Route path="/ButtonBasic" element={<ButtonBasic />} />
 
       </Routes>
     </BrowserRouter>
